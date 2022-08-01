6e76925e4634e814455d7359a823edc4d0aebb87
ARUZHAN AMANGELDINA <aruzhan.amangeldina@nu.edu.kz>
2022-08-02 01:31:43 +0600

    new commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import TextField from "@mui/material/TextField";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-import {Route, Routes} from 'react-router-dom';
-import Search from "../Search/Search";
 import './Home.css'
+import './Homemob.css'
 import FlipLogo from "../../assets/flip_logo.png";
 import MelomanLogo from "../../assets/meloman_logo.png";
 import WildLogo from "../../assets/wildberries_logo.png";
@@ -13,16 +11,20 @@ import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Logo from "../../assets/Logo.png";
 import {NavLink} from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 const Home = () => {
 
-  const handleSearch = () => {
-    <Routes>
-      <Route path="/allbooks" element={<Search/>} exact/>\
-    </Routes>
-  }
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 992px)"
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-width: 480px)"
+  });
 
   return (
+    <>
+    {isDesktop ?  
     <div className="container-home">
       
       <div className="inner-cont">
@@ -78,21 +80,9 @@ const Home = () => {
               <img style={{width:'25%', height:'auto'}} src={FlipLogo}/>
               <img style={{width:'33%', height:'auto'}} src={MelomanLogo}/>
             </div>
-            
-              {/* <img style={{width:'20%', height:'auto'}} src={FlipLogo}/>
-              <img style={{width:'20%', height:'auto'}} src={MelomanLogo}/>
-              <img style={{width:'20%', height:'auto'}} src={WildLogo}/> */}
             </div>
-            
-            
           </div>
-          
         </div>
-
-
-        
-        
-        
       </div>
         <div className="shelf">
             <div className="top"></div>
@@ -207,6 +197,211 @@ const Home = () => {
             </div>
         </div>
     </div>
+    : 
+    <div className="container-home-m">
+      <div className="inner-cont-m">
+        <div className='logo-m'>
+            {<img LinkComponent={NavLink} to="/home" 
+            style={{width:'10%', height:'auto'}} src={Logo}/>}
+            <Button 
+                sx={{
+                  maxWidth: '40%', 
+                  minWidth: '40%', 
+                  fontSize:'13px',
+                  borderColor: '#b0643e',
+                  color:'#b0643e',
+                  borderRadius: '50px',
+                  // width: '60%'
+                }}
+                variant="outlined"
+                component={Link} to="/allbooks"
+                  // target="_blank" 
+                  // InputProps={{endAdornment: 
+                  //   }}
+                >Начать  
+                <IconButton disabled="true" aria-label="search"  type="Submit">
+                    <SearchIcon style={{ fill: "#b0643e" }} />
+                </IconButton>
+              </Button>
+        </div>
+
+        <div className="row-1-m">
+          <div className='col-1-m'>
+            <div className="primary-text-m">
+              {`Быстро & Доступно`}
+            </div>
+            <div className="secondary-text-m">
+              {`Найди и сравнивай стоимость любимых книг с местных магазинов`}
+            </div>
+            <div className="search-bar">
+            </div>
+          </div>
+
+          <div className='col-2-m'>
+            <img className="intro-book-m" src={IntroBook}/>
+          </div>
+        </div>
+        
+      </div>
+
+        <div className="shelf-m">
+            <div className="top-m"></div>
+            <div className="front-m"></div>
+            <div className="shadow-m"></div>
+        </div>
+
+        <div className="under-shelf-m">
+        </div>
+
+        <div className="second-cont-m">
+          <div className='col-3-m'>
+            <div className='stores-text-m'>
+              Анализируй цены с магазинов:
+            </div>
+
+            <div className='stores-m'>
+            <div className='img-2-m'>
+              <img style={{width:'35%', height:'auto'}} src={WildLogo}/>
+            </div>
+            <div className='img-1-m'>
+              <img style={{width:'25%', height:'auto'}} src={FlipLogo}/>
+              <img style={{width:'33%', height:'auto'}} src={MelomanLogo}/>
+            </div>
+            </div>
+
+          </div>
+          <div className='under-col-m'>
+                <div className='under-col-img-m'>
+                  <img 
+                    src="//cdn.f.kz/prod/1834/1833874_550.jpg" 
+                    alt="flip-img"
+                    style={{
+                      width: '55%',
+                      height: 'auto',
+                      filter: 'drop-shadow(10px 10px 10px #c2b5a3)'
+                    }}/>
+                </div>
+                <div className='under-col-text-m'>
+                  <div className="text-wrapper-m">
+                  <div className='intro-book-title-m'>
+                    {'К себе нежно'}
+                  </div>
+                  <div className='intro-book-info-m'>
+                    {'Flip'}
+                  </div>
+                  <Button 
+                    sx={{
+                        borderColor: '#b0643e',
+                        color:'#b0643e',
+                        borderRadius: '50px',
+                        maxWidth: '60%', minWidth: '60%'
+                    }}
+                    variant="outlined"
+                    href={'https://www.flip.kz/catalog?prod=1833874'}  
+                    target="_blank" 
+                    >Купить
+                    </Button>
+                  </div>
+                </div>
+            </div>
+
+            <div className='under-col-m'>
+                <div className='under-col-img-m'>
+                  <img 
+                    src="https://simg.marwin.kz/media/catalog/product/cache/8d1771fdd19ec2393e47701ba45e606d/7/1/kolgan_dj_knijnyy_magazinchik_schastya.jpg" 
+                    alt="flip-img"
+                    style={{
+                      width: '55%',
+                      height: 'auto',
+                      filter: 'drop-shadow(10px 10px 10px #c2b5a3)'
+                    }}/>
+                </div>
+                <div className='under-col-text-m'>
+                  <div className="text-wrapper-m">
+                  <div className='intro-book-title-m'>
+                    {'Книжный магазинчик счастья'}
+                  </div>
+                  <div className='intro-book-info-m'>
+                    {'Meloman'}
+                  </div>
+                  <Button 
+                    sx={{
+                        borderColor: '#b0643e',
+                        color:'#b0643e',
+                        borderRadius: '50px',
+                        maxWidth: '60%', minWidth: '60%'
+                    }}
+                    variant="outlined"
+                    href={'https://www.flip.kz/catalog?prod=1833874'}  
+                    target="_blank" 
+                    >Купить
+                    </Button>
+                  </div>
+                </div>
+            </div>
+
+            <div className='under-col-m'>
+              <div className='under-col-img-m'>
+                  <img 
+                    src="//cdn.f.kz/prod/485/484566_550.jpg" 
+                    alt="flip-img"
+                    style={{
+                      width: '57%',
+                      height: 'auto',
+                      filter: 'drop-shadow(10px 10px 10px #c2b5a3)'
+                    }}/>
+                </div>
+                <div className='under-col-text-m'>
+                  <div className="text-wrapper-m">
+                  <div className='intro-book-title-m'>
+                    {'Мартин Иден'}
+                  </div>
+                  <div className='intro-book-info-m'>
+                    {'Wildberries'}
+                  </div>
+                  <Button 
+                    sx={{
+                        borderColor: '#b0643e',
+                        color:'#b0643e',
+                        borderRadius: '50px',
+                        maxWidth: '60%', minWidth: '60%'
+                    }}
+                    variant="outlined"
+                    href={'https://www.flip.kz/catalog?prod=1833874'}  
+                    target="_blank" 
+                    >Купить
+                    </Button>
+                  </div>
+                </div>
+            </div>
+
+          <div className='last-btn'>
+          <Button 
+                sx={{
+                  maxWidth: '33%', 
+                  minWidth: '33%', 
+                  fontSize:'13px',
+                  borderColor: '#b0643e',
+                  color:'#b0643e',
+                  borderRadius: '50px',
+                  // width: '60%'
+                }}
+                variant="outlined"
+                component={Link} to="/allbooks"
+                  // target="_blank" 
+                  // InputProps={{endAdornment: 
+                  //   }}
+                >Начать  
+                <IconButton disabled="true" aria-label="search"  type="Submit">
+                    <SearchIcon style={{ fill: "#b0643e" }} />
+                </IconButton>
+              </Button>
+          </div>
+        </div>
+
+    </div>
+    }
+  </>
   )
 }
 
