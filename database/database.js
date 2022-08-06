8548fe7f6b2463ac16b90219e84a602e590358bc
ARUZHAN AMANGELDINA <aruzhan.amangeldina@nu.edu.kz>
2022-08-06 13:12:07 +0600

    new commit

--- database/database.js
+++ database/database.js
@@ -2,6 +2,7 @@ const mongoose = require ('mongoose');
 
 const connectToDatabase = async () => {
     await mongoose.connect('mongodb+srv://aruzhan-amangeldina:6916qwopzxnm@oqu-project.kzzew.mongodb.net/library')
+    // await mongoose.connect('mongodb://localhost:27017', {family:4})
     .then(() => {
         console.log("successfully connected");
     })
